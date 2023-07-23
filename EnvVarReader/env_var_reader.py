de50112793040be34eecf874edf4383ae092e428
Andrew R <root.drew27@gmail.com>
2023-07-23 14:26:48 -0500

    Update env_var_reader.py

--- EnvVarReader/env_var_reader.py
+++ EnvVarReader/env_var_reader.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+import os
 import json
 from django.core.exceptions import ImproperlyConfigured
 
@@ -8,13 +10,15 @@ class Secrets():
         """Initialize a Secrets Instance   
 
         Args:
-            filename (str): a json file
+            filename (str): a json file (in the EnvVarReader dir)
         """
         try:
-            with open(filename) as f:
+            EnvVarReader_Dir = Path(__file__).resolve().parent
+            file_path = os.path.join(EnvVarReader_Dir, filename)
+            with open(file_path) as f:
                 self.secrets = json.loads(f.read())
         except OSError:
-            error_msg = "The file: {0} failed to open.. Has it been created?".format(filename)
+            error_msg = "The file: {0} failed to open.. Is it in the EnvVarReader directory?".format(filename)
             print(error_msg)
 
     def getSecret(self, setting:str):
