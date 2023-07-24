e7d92f8849b7ef63479f387bb3435f0f18892dbb
Andrew R <root.drew27@gmail.com>
2023-07-23 21:43:12 -0500

    Update settings.py
    added MEDIA_URL and MEDIA_ROOT

--- project_name/settings.py
+++ project_name/settings.py
@@ -122,6 +122,10 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'bootstrap'),
 ]
 
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 SESSION_ENGINE = "django.contrib.sessions.backends.file"
